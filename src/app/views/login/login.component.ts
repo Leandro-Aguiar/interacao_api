affbbe683273dfc44b24c5c0b62b1c52a2f3110c
Leandro <leosaguiarl@yahoo.com.br>
2023-01-11 17:20:23 -0300

    Concluindo atividade 2

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/model/user';
 import { LoginService } from 'src/app/services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,20 +10,38 @@ import { LoginService } from 'src/app/services/login.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private loginService: LoginService) { }
+  constructor(private loginService: LoginService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   userModel = new User();
+
+  mensagemerro = ""
+
+  mensagemacerto = ""
+  
+
   
   receberDados(){
 
     console.log(this.userModel)
 
     this.loginService.login(this.userModel).subscribe((response) => {
-      console.log(response)
-    })
+      console.log("Deu Certo")
+      this.mensagemerro = ''
+      localStorage.setItem("nomeUsuario", response.body.user.nome)
+      this.mensagemacerto = "Dados Corretos"
+     
+
+      
+
+    }, (respostaErro) => {
+      this.mensagemacerto = ''
+      console.log("Deu Erro")
+      this.mensagemerro = respostaErro.error
+      //alert(respostaErro.error)
+    } )
 
   }
 
